1d1ea3ca3c70d1a8d5f763368ab6f6b09a5a37ae
ismaeeladams <adamsismaeel387@gmail.com>
2022-07-11 14:29:57 +0200

    new things

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import { createStore } from "vuex";
 
 export default createStore({
+  // State is where the data lives 
   state: {
     items: [],
     users: [],
   },
   getters: {},
+  // Mutations are used to update state
   mutations: {},
+  // Actions are for ASYNC / Fetch calls
   actions: {},
   modules: {},
 });
