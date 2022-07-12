33fc128c489a149abf950fdcdf416dbb29970c84
Mika Rinquest <rinquestmika@gmail.com>
2022-07-12 11:07:00 +0200

    Added actions

--- src/store/index.js
+++ src/store/index.js
@@ -9,5 +9,49 @@ export default createStore({
   // Mutations are used to update state
   mutations: {},
   // Actions are for ASYNC / Fetch calls
-  actions: {},
+  actions: {
+    getItems: async (context) => {
+      fetch("http://localhost:3000/items")
+        .then((res) => res.json())
+        .then((items) => context.commit("setItems", items));
+
+      /* How to get the fetch data in the components
+      <div>
+      <ul>
+      <li v-for="item in items"> [Essentially a forEach function]
+        <p>{{item.id}}</p>
+      </li>
+      </ul>
+      </div>
+
+      <script>
+      export default {
+     mounted() {
+     this.$store.dispatch("getItems");
+     }
+        [this = refers to the component you're currently working in]
+        [$store = accesses the store folder]
+        [state = accesses the json file]
+        [dispatch =sends any changes made to the array]
+        }
+      }
+      }
+      computed; {
+     properties(){},
+     [This displays the array]
+     }
+      </script>
+      */
+    },
+    getUsers: async (context) => {
+      fetch(
+        `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.length) return (this.user = data[0]);
+          alert("No user found, please register");
+        });
+    },
+  },
 });
