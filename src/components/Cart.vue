26c915bfa64a756bd0104bec437a59a79222857c
TristanDaniels <tristandaniels07@gmail.com>
2022-07-12 14:57:27 +0200

    update

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,193 +1,187 @@
 <template>
-<body>
+  <body>
     <div class="wrapper">
-        <h1>Shopping Cart</h1>
-        <div class="project">
-            <div class="shop">
-                <div class="box">
-                    <img :src="item.image" alt="food">
-                    <div class="content">
-                        <h3>{{ item.name }}</h3>
-                        <h4>Price:R{{ item.price }}</h4>
-                        <p class="unit">Quantity:<input value="2"></p>
-                        <p class="btn-area">
-                            <i class="fa fa-trash"></i>
-                            <span class="btn2">Remove</span></p>
-                    </div>
-                </div>
-
+      <h1>Shopping Cart</h1>
+      <div class="project">
+        <div class="shop">
+          <div class="box">
+            <img src="item.image" alt="food" />
+            <div class="content">
+              <h3></h3>
+              <h4>Price:R</h4>
+              <p class="unit">Quantity:<input value="2" /></p>
+              <p class="btn-area">
+                <i class="fa fa-trash"></i>
+                <span class="btn2">Remove</span>
+              </p>
             </div>
-                    <div class="right-bar">
-                        <p><span>Subtotal</span> <span>R90.00</span></p>
-                        <hr>
-                        <p><span>Total</span> <span>R90.00</span></p>
-                        <a href="#"> <i class="fa fa-shopping-cart"></i>Checkout</a>
-
-                    </div>  
+          </div>
         </div>
+        <div class="right-bar">
+          <p><span>Subtotal</span> <span>R90.00</span></p>
+          <hr />
+          <p><span>Total</span> <span>R90.00</span></p>
+          <a href="#"> <i class="fa fa-shopping-cart"></i>Checkout</a>
+        </div>
+      </div>
     </div>
-</body>
+  </body>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
-*{
-    margin:0;
-    padding: 0;
-    box-sizing: border-box;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-body{
-    font-family: montserrat;
-    background: white;
+body {
+  font-family: montserrat;
+  background: white;
 }
 
-.wrapper{
-    max-width: 1000px;
-    margin: 0 auto;
+.wrapper {
+  max-width: 1000px;
+  margin: 0 auto;
 }
-.wrapper h1{
-    padding: 30px 0;
-    text-align: center;
-    text-transform: uppercase;
+.wrapper h1 {
+  padding: 30px 0;
+  text-align: center;
+  text-transform: uppercase;
 }
 
-.project{
-    display: flex;
+.project {
+  display: flex;
 }
-.shop{
-    flex: 75%;
+.shop {
+  flex: 75%;
 }
-.box{
-    display: flex;
-    width: 100%;
-    height: 200px;
-    overflow: hidden;
+.box {
+  display: flex;
+  width: 100%;
+  height: 200px;
+  overflow: hidden;
+  margin-bottom: 20px;
+  background: white;
+  transition: all 0.6s ease;
+  box-shadow: #42b983 (100, 100, 111, 0.2) 0.7px 29px 0;
+}
+.box:hover {
+  transform: scale(1.02);
+}
+
+.box img {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+}
+
+.content {
+  padding: 20px;
+  position: relative;
+  width: 100%;
+}
+
+.content h3 {
+  margin-bottom: 30px;
+}
+.content h4 {
+  margin-bottom: 50px;
+}
+
+.btn-area {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 25px;
+  background-color: #42b983;
+  color: white;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.btn-area:hover {
+  background-color: #42b983;
+  color: white;
+}
+
+.unit input {
+  width: 50px;
+  padding: 8px;
+  text-align: center;
+}
+
+.btn-area i {
+  margin-right: 5px;
+}
+
+.right-bar {
+  flex: 25%;
+  margin-left: 20px;
+  padding: 20px;
+  height: 400px;
+  background: white;
+  box-shadow: #42b983 (100, 100, 111, 0.2) 0.7px 29px 0;
+}
+.right-bar hr {
+  margin-bottom: 25px;
+}
+.right-bar p {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  font-size: 20px;
+}
+.right-bar a {
+  background-color: #42b983;
+  color: white;
+  text-decoration: none;
+  display: block;
+  text-align: center;
+  height: 40px;
+  font-weight: 900;
+}
+.right-bar i {
+  margin-right: 15px;
+}
+.right-bar a:hover {
+  background-color: #42b983;
+}
+
+@media screen and (max-width: 700px) {
+  .content h3 {
+    margin-bottom: 15px;
+  }
+  .content h4 {
     margin-bottom: 20px;
-    background: white;
-    transition: all .6s ease;
-    box-shadow: #42b983 (100,100,111,0.2) 0.7px 29px 0;
-}
-.box:hover{
-    transform: scale(1.02);
-}
-
-.box img{
-    width: 300px;
-    height: 200px;
-    object-fit: cover;
-}
-
-.content{
-    padding: 20px;
-    position: relative;
-    width: 100%;
+  }
+  .btn2 {
+    display: none;
+  }
+  .box {
+    height: 150px;
+  }
+  .box img {
+    height: 150px;
+    width: 200px;
+  }
 }
 
-.content h3{
-    margin-bottom: 30px;
-}
-.content h4{
-    margin-bottom: 50px;
-}
-
-.btn-area{
-    position: absolute;
-    bottom: 20px;
-    right: 20px;
-    padding: 10px 25px;
-    background-color: #42b983;
-    color: white;
-    cursor: pointer;
-    border-radius: 10px;
+@media screen and (max-width: 900px) {
+  .project {
+    flex-direction: column;
+  }
+  .right-bar {
+    margin-left: 0;
+    margin-bottom: 20px;
+  }
 }
 
-.btn-area:hover{
-    background-color: #42b983;
-    color: white;
+@media screen and (max-width: 125px) {
+  .wrapper {
+    max-width: 95%;
+  }
 }
-
-.unit input{
-    width: 50px;
-    padding: 8px;
-    text-align: center;
-}
-
-.btn-area i{
-    margin-right: 5px;
-}
-
-.right-bar{
-    flex: 25%;
-    margin-left: 20px;
-    padding: 20px;
-    height: 400px;
-    background: white;
-    box-shadow: #42b983 (100,100,111,0.2) 0.7px 29px 0;
-}
-.right-bar hr{
-    margin-bottom: 25px;
-}
-.right-bar p{
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 30px;
-    font-size: 20px;
-}
-.right-bar a{
-    background-color: #42b983;
-    color: white;
-    text-decoration: none;
-    display: block;
-    text-align: center;
-    height: 40px;
-    font-weight: 900;
-}
-.right-bar i{
-    margin-right: 15px;
-}
-.right-bar a:hover{
-    background-color: #42b983;
-}
-
-
-@media screen and (max-width:700px){
-    .content h3{
-        margin-bottom: 15px;
-    }
-    .content h4{
-        margin-bottom: 20px;
-    }
-    .btn2{
-        display: none;
-    }
-    .box{
-        height: 150px;
-    }
-    .box img{
-        height: 150px;
-        width: 200px;
-    }
-}
-
-@media screen and (max-width:900px) {
-    .project{
-        flex-direction: column;
-    }
-    .right-bar{
-        margin-left: 0;
-        margin-bottom: 20px;
-    }
-}
-
-@media screen and (max-width: 125px){
-    .wrapper{
-        max-width: 95%;
-    }
-}
-
-
-
-
 </style>
---
+++ src/views/Sign-inView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="SignIn">
+    <SignIn />
+  </div>
+</template>
+<script>
+import SignIn from "@/components/SignIn.vue";
+
+export default {
+  name: "SignInView",
+  components: {
+    SignIn,
+  },
+};
+</script>
