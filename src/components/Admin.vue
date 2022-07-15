0260d89d7e2d0412bf273355330ce020b6653280
Mika Rinquest <rinquestmika@gmail.com>
2022-07-15 13:51:11 +0200

    test

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -9,7 +9,7 @@
     <option value="Drinks">Drink</option>
     <option value="Sides">Side</option>
   </select>
-  <button @click="modal">Add</button>
+
   <table class="table">
     <thead>
       <tr>
@@ -20,7 +20,6 @@
         <th scope="col">Price</th>
         <th scope="col">Category</th>
         <th scope="col">Img URL</th>
-        <th>Edit</th>
         <th>Delete</th>
       </tr>
     </thead>
@@ -33,8 +32,8 @@
         <td>{{ item.price }}</td>
         <td>{{ item.category }}</td>
         <td>{{ item.image }}</td>
-        <td><i class="fa-solid fa-trash-can"></i></td>
-        <td><i class="fa-solid fa-pen-clip"></i></td>
+        <td><i class="fa-solid fa-trash-can" @click="deleteItem(id)"></i></td>
+        <td></td>
       </tr>
     </tbody>
   </table>
@@ -56,8 +55,6 @@ export default {
       return this.$store.state.user; //Pulls the array from the store
     },
     items() {
-      // return this.$store.state.items?.filter((item) => {
-      //   return item.name?.toLowerCase().includes(this.search.toLowerCase());
       return this.$store.state.items?.filter((item) => {
         let isMatch = true;
         if (!item.name.toLowerCase().includes(this.search.toLowerCase()))
@@ -70,7 +67,6 @@ export default {
   },
 
   mounted() {
-    this.$store.dispatch("getUser");
     this.$store.dispatch("getItems");
   },
 
@@ -84,59 +80,14 @@ export default {
         } /*This object becomes the payload. The payload will be compared to the original array to see if the inputs match*/
       );
     },
-    modal() {},
+
+    deleteModal(id) {
+      this.$store.dispatch("deleteFromCart");
+    },
   },
 };
 </script>
 <style scoped>
-<<<<<<< HEAD
-=======
-form {
-  height: 100vh;
-}
-/* Login */
-form {
-  border: 3px solid #f1f1f1;
-}
-input[type="text"],
-input[type="password"] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-button {
-  background-color: #42b983;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-button:hover {
-  opacity: 0.8;
-}
-.container {
-  padding: 16px;
-}
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-@media screen and (max-width: 300px) {
-  span.psw {
-    display: block;
-    float: none;
-  }
-  .cancelbtn {
-    width: 100%;
-  }
-}
-
->>>>>>> f3977f8aea8f259736984b98084b2461c10d89ce
 /* Admin */
 
 .image {
